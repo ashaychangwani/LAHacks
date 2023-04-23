b7933d845b16f5e730c8db5e802b0780d491197b
yash07007 <yashsolanki777@gmail.com>
2023-04-23 05:37:42 -0700

    user service test prod seperation

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -15,59 +15,72 @@ import { Observable } from 'rxjs';
 export class UserService {
 	backend_url = 'http://128.122.49.69:20440';
 	// backend_url = 'http://192.168.65.207:8000';
+	test = false;
 
 	constructor(private http: HttpClient) {}
 
 	// get list of all sessions
 	getStudySessions(user_email: string) {
-		let queryParams = new HttpParams();
-		queryParams = queryParams.append('user_id', user_email);
-		// return this.http.get<StudySession[]>(
-		// 	this.backend_url + '/get-sessions',
-		// 	{
-		// 		params: queryParams,
-		// 	}
-		// );
-		return this.http.get<StudySession[]>('/assets/sessions.json');
+		if (!this.test) {
+			let queryParams = new HttpParams();
+			queryParams = queryParams.append('user_id', user_email);
+			return this.http.get<StudySession[]>(
+				this.backend_url + '/get-sessions',
+				{
+					params: queryParams,
+				}
+			);
+		} else {
+			return this.http.get<StudySession[]>('/assets/sessions.json');
+		}
 	}
 
 	// get details of a specific session with notes
 	getStudySessionNotes(user_email: string, session_id: string) {
-		let queryParams = new HttpParams();
-		queryParams = queryParams.append('user_id', user_email);
-		queryParams = queryParams.append('session_id', session_id);
-		// return this.http.get<StudySessionDetail>(
-		// 	this.backend_url + '/get-session',
-		// 	{
-		// 		params: queryParams,
-		// 	}
-		// );
-		return this.http.get<StudySessionDetail>('/assets/notes.json');
+		if (!this.test) {
+			let queryParams = new HttpParams();
+			queryParams = queryParams.append('user_id', user_email);
+			queryParams = queryParams.append('session_id', session_id);
+			return this.http.get<StudySessionDetail>(
+				this.backend_url + '/get-session',
+				{
+					params: queryParams,
+				}
+			);
+		} else {
+			return this.http.get<StudySessionDetail>('/assets/notes.json');
+		}
 	}
 
 	// get quiz questions & answers for a specific session
 	getQuiz(user_email: string, session_id: string) {
-		let queryParams = new HttpParams();
-		queryParams = queryParams.append('user_id', user_email);
-		queryParams = queryParams.append('session_id', session_id);
-		// return this.http.get<Quiz>(this.backend_url + '/questions', {
-		// 	params: queryParams,
-		// });
-		return this.http.get<Quiz>('/assets/quiz.json');
+		if (!this.test) {
+			let queryParams = new HttpParams();
+			queryParams = queryParams.append('user_id', user_email);
+			queryParams = queryParams.append('session_id', session_id);
+			return this.http.get<Quiz>(this.backend_url + '/questions', {
+				params: queryParams,
+			});
+		} else {
+			return this.http.get<Quiz>('/assets/quiz.json');
+		}
 	}
 
 	// generate quiz questions & answers for a specific session
 	generateQuiz(user_email: string, session_id: string) {
-		// return this.http.post<SuccessResponse>(
-		// 	this.backend_url + '/generate-questions',
-		// 	{
-		// 		user_id: user_email,
-		// 		session_id: session_id,
-		// 	}
-		// );
-		return {
-			status: 'ok',
-		};
+		if (!this.test) {
+			return this.http.post<SuccessResponse>(
+				this.backend_url + '/generate-questions',
+				{
+					user_id: user_email,
+					session_id: session_id,
+				}
+			);
+		} else {
+			return {
+				status: 'ok',
+			};
+		}
 	}
 
 	getFeedback(
@@ -76,15 +89,18 @@ export class UserService {
 		quiz_question: QuizQuestion,
 		provided_attempt: string[]
 	) {
-		// return this.http.post<string>(this.backend_url + '/feedback', {
-		// 	user_id: user_email,
-		// 	session_id: session_id,
-		// 	question: quiz_question.question,
-		// 	reference_answer: quiz_question.answer,
-		// 	chosen_answer: provided_attempt,
-		// 	context: quiz_question.context,
-		// 	references: quiz_question.references,
-		// });
-		return this.http.get<string>('/assets/feedback.json');
+		if (!this.test) {
+			return this.http.post<string>(this.backend_url + '/feedback', {
+				user_id: user_email,
+				session_id: session_id,
+				question: quiz_question.question,
+				reference_answer: quiz_question.answer,
+				chosen_answer: provided_attempt,
+				context: quiz_question.context,
+				references: quiz_question.references,
+			});
+		} else {
+			return this.http.get<string>('/assets/feedback.json');
+		}
 	}
 }
