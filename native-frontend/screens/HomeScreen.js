c97e340dc779d8f8d9e4790e702b08b19f66eb46
Ashay Changwani <ashaychangwani@gmail.com>
2023-04-22 23:06:14 -0700

    don't allow user to go back to login if they back

--- native-frontend/screens/HomeScreen.js
+++ native-frontend/screens/HomeScreen.js
@@ -8,10 +8,11 @@ import * as Google from "expo-auth-session/providers/google";
 WebBrowser.maybeCompleteAuthSession();
 
 
-function HomeScreen({route,navigation}) {
-  const [token, setToken] = useState("");
-  const [userInfo, setUserInfo] = useState(null);
-  const [email, setEmail] = useState(null);
+function HomeScreen({ route, navigation }) {
+  const [ token, setToken ] = useState("");
+  const [ userInfo, setUserInfo ] = useState(null);
+  const [ email, setEmail ] = useState(null);
+
   const baseUrl = 'http://128.122.49.69:20440/'
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: "48155888637-grafopqvvm80gp3qo6vfptu40bti1a33.apps.googleusercontent.com",
@@ -26,6 +27,20 @@ function HomeScreen({route,navigation}) {
     }
   }, [response, token]);
 
+  const navigateToSessions = () => {
+    console.log("Navigating to Sessions")
+    navigation.navigate("SessionsScreen", {email: email, baseUrl})
+  }
+
+  useEffect(() => {
+    console.log("INside useEffect")
+    const unsubscribe = navigation.addListener('focus', () => {
+      if(email != null)
+        navigateToSessions()
+    });
+    return unsubscribe;
+  }, [navigation, email])
+
   const getUserInfo = async () => {
     try {
       const response = await fetch(
@@ -37,8 +52,8 @@ function HomeScreen({route,navigation}) {
 
       const user = await response.json();
       setEmail(user.email);
+      navigateToSessions()
       setUserInfo(user);
-      navigation.navigate("SessionsScreen", {email: user.email, baseUrl})
     } catch (error) {
       // Add your own error handler here
     }
