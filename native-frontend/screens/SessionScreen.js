96a810d4ab7301ab5b69fe6ef1c496ea7c320ed2
Ashay Changwani <ashaychangwani@gmail.com>
2023-04-23 00:25:36 -0700

    incomplete session screen, fix quiz func

--- native-frontend/screens/SessionScreen.js
+++ native-frontend/screens/SessionScreen.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Button, RefreshControl } from "react-native";
+import { StyleSheet, Text, View, Button, RefreshControl, Pressable } from "react-native";
 import { FlatList } from 'react-native-gesture-handler';
 
 function SessionScreen({route, navigation}) {
@@ -56,7 +56,7 @@ function SessionScreen({route, navigation}) {
                     ))}
                 </View>
             )
-            
+
         return (
             <View className="w-full bg-gray-500 h-5">
                 <Text>{item.type}</Text>
@@ -71,20 +71,61 @@ function SessionScreen({route, navigation}) {
         getSessionData();
     }
 
+    const generateQuiz = async () => {
+        try {
+            const session = {
+                session_id: session_id,
+                user_id: email
+            };
+            const num_questions = 20;
+            const response = await fetch(`${baseUrl}generate-questions`, {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                session: session,
+                num_questions: num_questions
+                })
+            });
+        }
+        catch (error){}
+    }
+
+    const navigateToQuiz = () => {
+        // navigation.navigate('Quiz',
+    }
+
     return (
         <View className="flex flex-1 ">
             { sessionData != null ? (
                 <View>
-                    <Text>{sessionData.session_name}</Text>
-                    <FlatList
-                        data={sessionData.blobs}
-                        renderItem={({item}) => renderItem(item)}
-                        // Use index for key
-                        keyExtractor={(item, index) => index.toString()}
-                        refreshControl={
-                            <RefreshControl refreshing={fetching} onRefresh={onRefresh} />
-                        }
-                        />
+                    <View>
+                        <Text>{sessionData.session_name}</Text>
+                        <FlatList
+                            data={sessionData.blobs}
+                            renderItem={({item}) => renderItem(item)}
+                            // Use index for key
+                            keyExtractor={(item, index) => index.toString()}
+                            refreshControl={
+                                <RefreshControl refreshing={fetching} onRefresh={onRefresh} />
+                            }
+                            />
+                    </View>
+                { 
+                    Object.keys(sessionData.quiz).length == 0 ? 
+                        (
+                            <Pressable onPress={()=>generateQuiz}>
+                                <Text>Create Quiz</Text>
+                            </Pressable>
+                        )
+                        :
+                        (
+                            <Pressable onPress={()=>navigateToQuiz}>
+                                <Text>Take Quiz ({sessionData.quiz.num_questions} questions)</Text>
+                            </Pressable>
+                        )
+                }
                 </View>
                 ) : (
                     <View></View>
