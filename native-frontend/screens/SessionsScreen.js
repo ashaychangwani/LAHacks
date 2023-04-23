07c01012c1454c372aaa4450ca5b3c05ee6fa4bc
Ashay Changwani <ashaychangwani@gmail.com>
2023-04-22 22:53:15 -0700

    Added pull to refresh

--- native-frontend/screens/SessionsScreen.js
+++ native-frontend/screens/SessionsScreen.js
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, RefreshControl } from "react-native";
 import { FlatList } from 'react-native-gesture-handler';
 
 function SessionsScreen({route, navigation}) {
     const { email, baseUrl } = route.params;
     const [sessions, setSessions] = useState([]);
-
+    const [fetching, setFetching] = useState(false);
     useEffect(() => {
         console.log(email)
         if(email != null)
@@ -27,24 +27,34 @@ function SessionsScreen({route, navigation}) {
             setSessions(sessions);
         } catch (error) {
             // Add your own error handler here
+        } finally {
+            setFetching(false);
         }
     }
 
     const renderItem = (item) => {
         console.log("Rendering item")
         return (
-            <View>
+            <View className="w-full bg-gray-500 h-5">
                 <Text>{item.session_id}</Text>
                 <Text>{item.session_name}</Text>
             </View>
         )
     }
+
+    const onRefresh = () => {
+        console.log("Refreshing");
+        setFetching(true)
+        getSessions();
+    }
+
     return (
-        <View className="flex flex-1 bg-[#F00]">
+        <View className="flex flex-1">
             <FlatList
                 data={sessions}
                 renderItem={({item}) => renderItem(item)}
                 keyExtractor={item => item.session_id}
+                refreshControl={<RefreshControl refreshing={fetching} onRefresh={onRefresh} />}
             />
 
         </View>
